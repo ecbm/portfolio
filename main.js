e9d69887918f8e07be923eee160a1c739f734927
Eli Markham <em@r4b.app>
2022-04-26 18:24:43 +0100

    minor changes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import './style.css'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+
