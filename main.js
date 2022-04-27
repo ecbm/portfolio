466296c5ab813bdef55f3d97bdef1fad5a5d23a0
ecbm.r4b <em@r4b.app>
2022-04-27 10:40:08 +0100

    three js basic set up

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 import './style.css'
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+const material = new THREE.MeshStandardMaterial({color: 0xff2567});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(20,20,20);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(ambientLight, pointlight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
+function animate() {
+  requestAnimationFrame(animate);
+
+  //torus.rotation.x += 0.01;
+  //torus.rotation.y += 0.005;
+  //torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate()
+
+renderer.render(scene, camera);
